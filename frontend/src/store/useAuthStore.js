0038b712037bbafaf6267261ff7b16faae4aaa4f
ShriyanshJaiswal01 <shriyanshmgs235@gmail.com>
2025-12-10 10:01:32 +0530

    Fix render deployment issues

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,12 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://justchat-39q3.onrender.com";
+
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
